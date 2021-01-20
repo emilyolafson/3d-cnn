cdab1ad02130c750fe3aa9090e185f27109662c8
Emily Olafson <emily.olafson@mail.mcgill.ca>
2021-01-19 21:02:47 -0800

    fix bug

--- 3dchaco_code.py
+++ 3dchaco_code.py
@@ -57,36 +57,37 @@ def validation_preprocessing(volume, label):
     volume = tf.expand_dims(volume, axis=3)
     return volume, label
 
- def get_model(width=64, height=64, depth=64):
-        """Build a 3D convolutional neural network model."""
+def get_model(width=64, height=64, depth=64):
+    """Build a 3D convolutional neural network model."""
 
-        inputs = keras.Input((width, height, depth, 1))
+    inputs = keras.Input((width, height, depth, 1))
 
-        x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
-        x = layers.MaxPool3D(pool_size=2)(x)
-        x = layers.BatchNormalization()(x)
+    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
 
-        x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
-        x = layers.MaxPool3D(pool_size=2)(x)
-        x = layers.BatchNormalization()(x)
+    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
 
-        x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
-        x = layers.MaxPool3D(pool_size=2)(x)
-        x = layers.BatchNormalization()(x)
+    x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
 
-        x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
-        x = layers.MaxPool3D(pool_size=2)(x)
-        x = layers.BatchNormalization()(x)
+    x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
+    x = layers.MaxPool3D(pool_size=2)(x)
+    x = layers.BatchNormalization()(x)
 
-        x = layers.GlobalAveragePooling3D()(x)
-        x = layers.Dense(units=512, activation="relu")(x)
-        x = layers.Dropout(0.3)(x)
+    x = layers.GlobalAveragePooling3D()(x)
+    x = layers.Dense(units=512, activation="relu")(x)
+    x = layers.Dropout(0.3)(x)
 
-        outputs = layers.Dense(units=1, activation="sigmoid")(x)
+    outputs = layers.Dense(units=1, activation="sigmoid")(x)
 
-        # Define the model.
-        model = keras.Model(inputs, outputs, name="3dcnn")
-        return model
+    # Define the model.
+    model = keras.Model(inputs, outputs, name="3dcnn")
+    return model
+
     
 METRICS = [
     keras.metrics.BinaryAccuracy(name='accuracy'),
