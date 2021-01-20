a96b49aa42937daef819bece07675580760bbe3e
Emily Olafson <emily.olafson@mail.mcgill.ca>
2021-01-19 20:46:40 -0800

    kfold

--- 3dchaco_code.py
+++ 3dchaco_code.py
@@ -57,8 +57,45 @@ def validation_preprocessing(volume, label):
     volume = tf.expand_dims(volume, axis=3)
     return volume, label
 
+ def get_model(width=64, height=64, depth=64):
+        """Build a 3D convolutional neural network model."""
+
+        inputs = keras.Input((width, height, depth, 1))
+
+        x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
+        x = layers.MaxPool3D(pool_size=2)(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
+        x = layers.MaxPool3D(pool_size=2)(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
+        x = layers.MaxPool3D(pool_size=2)(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
+        x = layers.MaxPool3D(pool_size=2)(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.GlobalAveragePooling3D()(x)
+        x = layers.Dense(units=512, activation="relu")(x)
+        x = layers.Dropout(0.3)(x)
+
+        outputs = layers.Dense(units=1, activation="sigmoid")(x)
+
+        # Define the model.
+        model = keras.Model(inputs, outputs, name="3dcnn")
+        return model
+    
+METRICS = [
+    keras.metrics.BinaryAccuracy(name='accuracy'),
+    keras.metrics.AUC(name='auc'),
+]
+
 skf = StratifiedKFold(n_splits=10, random_state=7, shuffle=True)
 
+skf_count = 0
 for train_idx, val_idx in skf.split(x,y):
     print("train: ", train_idx, " val: ", val_idx)
     x_train = x[train_idx]
@@ -67,132 +104,89 @@ for train_idx, val_idx in skf.split(x,y):
     y_val = y[val_idx]
     print(y_val)
 
-# Define data loaders.
-train_loader = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-validation_loader = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    # Define data loaders.
+    train_loader = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    validation_loader = tf.data.Dataset.from_tensor_slices((x_val, y_val))
 
-batch_size = 2
-# Augment the on the fly during training.
-train_dataset = (
-    train_loader.shuffle(len(x_train))
-    .map(train_preprocessing)
-    .batch(batch_size)
-    .prefetch(2)
-)
-# Only rescale.
-validation_dataset = (
-    validation_loader.shuffle(len(x_val))
-    .map(validation_preprocessing)
-    .batch(batch_size)
-    .prefetch(2)
-)
-
-
-def get_model(width=64, height=64, depth=64):
-    """Build a 3D convolutional neural network model."""
-
-    inputs = keras.Input((width, height, depth, 1))
-
-    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
-    x = layers.MaxPool3D(pool_size=2)(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
-    x = layers.MaxPool3D(pool_size=2)(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
-    x = layers.MaxPool3D(pool_size=2)(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
-    x = layers.MaxPool3D(pool_size=2)(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.GlobalAveragePooling3D()(x)
-    x = layers.Dense(units=512, activation="relu")(x)
-    x = layers.Dropout(0.3)(x)
-
-    outputs = layers.Dense(units=1, activation="sigmoid")(x)
-
-    # Define the model.
-    model = keras.Model(inputs, outputs, name="3dcnn")
-    return model
-
-
-# Build model.
-model = get_model(width=64, height=64, depth=64)
-model.summary()
-
-METRICS = [
-      keras.metrics.TruePositives(name='tp'),
-      keras.metrics.FalsePositives(name='fp'),
-      keras.metrics.TrueNegatives(name='tn'),
-      keras.metrics.FalseNegatives(name='fn'),
-      keras.metrics.BinaryAccuracy(name='accuracy'),
-      keras.metrics.Precision(name='precision'),
-      keras.metrics.Recall(name='recall'),
-      keras.metrics.AUC(name='auc'),
-]
-
-# Compile model.
-initial_learning_rate = 0.0001
-lr_schedule = keras.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
-)
-
-model.compile(
-    loss="binary_crossentropy",
-    optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
-    metrics=[METRICS, "binary_crossentropy"]
-)
-
-# Define callbacks.
-checkpoint_cb = keras.callbacks.ModelCheckpoint("3d_image_classification.h5", save_best_only=True)
-
-early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_loss",mode="min", patience=5)
-
-# Train the model, doing validation at the end of each epoch
-epochs = 100
-model.fit(
-    train_dataset,
-    validation_data=validation_dataset,
-    epochs=epochs,
-    shuffle=True,
-    verbose=1,
-    callbacks=[checkpoint_cb, early_stopping_cb],
-)
-
-results_dir = str(cwd) + "/results"
-model_json=model.to_json()
-with open(results_dir + "model.json", "w") as json_file:
-    json_file.write(model_json)
-
-model.save_weights(results_dir + "/model.h5")
-print("saved model to disk")
-
-fig, ax = plt.subplots(1, 2, figsize=(20, 3))
-ax = ax.ravel()
-
-for i, metric in enumerate(["auc", "loss"]):
-    ax[i].plot(model.history.history[metric])
-    ax[i].plot(model.history.history["val_" + metric])
-    ax[i].set_title("Model {}".format(metric))
-    ax[i].set_xlabel("epochs")
-    ax[i].set_ylabel(metric)
-    ax[i].legend(["train", "val"])
-
-plt.savefig(results_dir + "/acc_loss_QSM_MSConnect_2mm.png")
-
-# Load best weights.
-model.load_weights("3d_image_classification.h5")
-prediction = model.predict(np.expand_dims(x_test[0], axis=0))[0]
-scores = [1 - prediction[0], prediction[0]]
-
-class_names = ["normal", "disabled"]
-for score, name in zip(scores, class_names):
-    print(
-        "This model is %.2f percent confident that MRI scan is %s"
-        % ((100 * score), name)
+    batch_size = 2
+    # Augment the on the fly during training.
+    train_dataset = (
+        train_loader.shuffle(len(x_train))
+        .map(train_preprocessing)
+        .batch(batch_size)
+        .prefetch(2)
     )
+    # Only rescale.
+    validation_dataset = (
+        validation_loader.shuffle(len(x_val))
+        .map(validation_preprocessing)
+        .batch(batch_size)
+        .prefetch(2)
+    )
+
+    # Build model.
+    model = get_model(width=64, height=64, depth=64)
+    model.summary()
+
+    # Compile model.
+    initial_learning_rate = 0.0001
+    lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
+    )
+
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
+        metrics=[METRICS, "binary_crossentropy"]
+    )
+
+    # Define callbacks.
+    checkpoint_cb = keras.callbacks.ModelCheckpoint(results_dir + "/model_" + str(skf_count) + ".h5", save_best_only=True)
+
+    early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_auc", patience=15)
+
+    # Train the model, doing validation at the end of each epoch
+    epochs = 100
+    model.fit(
+        train_dataset,
+        validation_data=validation_dataset,
+        epochs=epochs,
+        shuffle=True,
+        verbose=1,
+        callbacks=[checkpoint_cb, early_stopping_cb],
+    )
+
+    results_dir = str(cwd) + "/results"
+    model_json=model.to_json()
+    with open(results_dir + "model_" + str(skf_count)+".json", "w") as json_file:
+        json_file.write(model_json)
+
+    model.save_weights(results_dir + "/model_" + str(skf_count) + ".h5")
+    print("saved model to disk")
+
+    fig, ax = plt.subplots(1, 2, figsize=(20, 3))
+    ax = ax.ravel()
+
+    for i, metric in enumerate(["auc", "loss"]):
+        ax[i].plot(model.history.history[metric])
+        ax[i].plot(model.history.history["val_" + metric])
+        ax[i].set_title("Model {}".format(metric))
+        ax[i].set_xlabel("epochs")
+        ax[i].set_ylabel(metric)
+        ax[i].legend(["train", "val"])
+
+    plt.savefig(results_dir + "/auc_loss_" + str(skf_count) +".png")
+
+    # Load best weights.
+    model.load_weights(results_dir + "/model_" + str(skf_count) + ".h5")
+    prediction = model.predict(np.expand_dims(x_val[0], axis=0))[0]
+    scores = [1 - prediction[0], prediction[0]]
+
+    class_names = ["normal", "disabled"]
+    for score, name in zip(scores, class_names):
+        print(
+            "This model is %.2f percent confident that MRI scan is %s"
+            % ((100 * score), name)
+        )
+    skf_count = skf_count + 1
 
--- import_pkl.py
+++ import_pkl.py
@@ -2,11 +2,9 @@ import pickle
 import numpy
 import scipy.io as sio
 
+
 for i in range(1,23):
-    with open('/Users/emilyolafson/Documents/Thesis/colossus_syncs/SUB1_23_data/NeMo_SUB1_23/shen_regional/SUB' + str(i) + '_lesion_1mmMNI_nemo_output_chacovol_shen268_mean.pkl', 'r+b') as f:
-        data = pickle.load(f)
-        numpy.savetxt('SUB' + str(i) + '_lesion_1mmMNI_shen268_mean_chacovol.csv', data, delimiter=",")
-    with open('/Users/emilyolafson/Documents/Thesis/colossus_syncs/SUB1_23_data/NeMo_SUB1_23/shen_regional/SUB' + str(i) + '_lesion_1mmMNI_nemo_output_chacoconn_shen268_mean.pkl', 'r+b') as e:
+    with open('/Users/emilyolafson/GIT/fc-from-sc/SUB' + str(i) + '_lesion1mm_nemo_output_chacoconn_fs86subj_mean.pkl', 'r+b') as e:
+
         data2 = pickle.load(e)
-        print(data2.shape)
-        sio.savemat('SUB' + str(i) +'_lesion_1mmMNI_shen268_mean_chacoconn.mat', {'SUB'+str(i)+'chacoconn':data2})
+        sio.savemat('SUB' + str(i) +'_lesion_1mmMNI_fs86subj_mean_chacoconn.mat', {'SUB'+str(i)+'chacoconn':data2})
